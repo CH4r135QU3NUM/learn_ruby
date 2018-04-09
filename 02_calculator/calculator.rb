b97cbec8d99abe0ca51b069b3d7eb7f5aed7d96d
charles <charles.quenum@gmail.com>
2018-04-09 14:17:05 +0200

    version factorisé

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,21 +1,33 @@
 #write your code here
 def add (x, y)
-	if x==0 and y==0 then add = 0
-	elsif x==2 and y==2 then add = 4
-	elsif x==2 and y==6 then add = 8
-	else add = x + y 
-	end
+	add = x + y
 end
 
 def subtract (x, y)
-	if x==10 and y==4 then subtract = 6
-	end
+subtract = x - y
+
 end
 
 def sum(array)
-	if array==[] then sum = 0
-	elsif array==[7] then sum = 7
-	elsif array==[7, 11] then sum = 18	
-	elsif array==[1, 3, 5, 7 , 9] then sum = 25
+	array.inject(0){|sum,x| sum + x }
 end
+
+=begin
+def add (x, y)
+if x==0 and y==0 then add = 0
+elsif x==2 and y==2 then add = 4
+elsif x==2 and y==6 then add = 8
+else add = x + y
 end
+
+def subtract (x, y)
+if x==10 and y==4 then subtract = 6
+end
+
+def sum(array)
+if array==[] then sum = 0
+elsif array==[7] then sum = 7
+elsif array==[7, 11] then sum = 18
+elsif array==[1, 3, 5, 7 , 9] then sum = 25
+
+=end
