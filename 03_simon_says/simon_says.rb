adc1221ee94c21cbca345d3e2adab9a751cf5e63
charles <charles.quenum@gmail.com>
2018-04-09 17:05:34 +0200

    capitalize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -7,6 +7,17 @@ def shout(string)
 end
 
 def repeat(string, i=2)
-  string + (" #{string}" * (i-1))   
+  string + (" #{string}" * (i-1))
 end
 
+def start_of_word (string,n)
+  string[0..(n-1)]
+end
+
+def first_word(string)
+string.split.first
+end
+
+def titleize(string)
+  string.capitalize
+end
