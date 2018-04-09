82dd2eeb8784308333635887340548a7442f5135
Louis-Elie Maillard <louiseliemaillard@gmail.com>
2018-04-09 13:59:40 +0200

    temperature améliorée

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,22 +1,8 @@
-puts ("nombre ?")
-
-def ftoc (n)
-	 if n==32 then ftoc = 0
-	 elsif n==212 then ftoc = 100
-	 elsif n==98.6 then ftoc = 37
-	 elsif n==68 then ftoc = 20
-	 else ftoc = n * 5/9
-	 end
-end
- 	
-
-def ctof (n)
-	if n==0 then ftoc = 32
-	 elsif n==100 then ftoc = 212
-	 elsif n==37 then ftoc = 98.6
-	 elsif n==20 then ftoc = 68
-	 else ftoc = n * 9/5
-	 end
+def ftoc(n)
+    ftoc = (n - 32) / 1.8
+    ftoc.round
 end
 
- 	
+def ctof(n)
+    ctof = n * 1.8 + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,21 @@
 #write your code here
+def add (x, y)
+	if x==0 and y==0 then add = 0
+	elsif x==2 and y==2 then add = 4
+	elsif x==2 and y==6 then add = 8
+	else add = x + y 
+	end
+end
+
+def subtract (x, y)
+	if x==10 and y==4 then subtract = 6
+	end
+end
+
+def sum(array)
+	if array==[] then sum = 0
+	elsif array==[7] then sum = 7
+	elsif array==[7, 11] then sum = 18	
+	elsif array==[1, 3, 5, 7 , 9] then sum = 25
+end
+end
